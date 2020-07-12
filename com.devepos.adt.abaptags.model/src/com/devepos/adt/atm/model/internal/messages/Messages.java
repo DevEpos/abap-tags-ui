45f0ee86a40e91db3d0e00acd5c527a675c56db0
stockbal <ludwig.stockbauer-muhr@msg.group>
2020-07-12 17:59:54 +0200

    fix: correct message key mismatch

--- com.devepos.adt.abaptags.model/src/com/devepos/adt/atm/model/internal/messages/Messages.java
+++ com.devepos.adt.abaptags.model/src/com/devepos/adt/atm/model/internal/messages/Messages.java
@@ -5,7 +5,6 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "com.devepos.adt.atm.model.internal.messages.messages"; //$NON-NLS-1$
 	public static String TagListValidator_TagLengthInvalid_xmsg;
-	public static String TagListValidator_TagNameInvalid_xmsg;
 	public static String TagListValidator_DuplicateTagNameFound_xmsg;
 	public static String TagListValidator_InvalidCharactersInTag_xmsg;
 	public static String TagListValidator_NoTagName_xmsg;
