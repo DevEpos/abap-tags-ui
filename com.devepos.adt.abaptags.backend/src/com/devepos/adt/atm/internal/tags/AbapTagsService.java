d0ea4b00edb5fba954f034cc5e3c6a903d1196c0
stockbal <stocki87@gmail.com>
2021-04-11 22:07:37 +0200

    chore: code clean-up

--- com.devepos.adt.abaptags.backend/src/com/devepos/adt/atm/internal/tags/AbapTagsService.java
+++ com.devepos.adt.abaptags.backend/src/com/devepos/adt/atm/internal/tags/AbapTagsService.java
@@ -51,8 +51,8 @@ public class AbapTagsService implements IAbapTagsService {
         if (new AbapTagsUriDiscovery(destinationId).isResourceDiscoverySuccessful()) {
             return Status.OK_STATUS;
         }
-        return new Status(IStatus.ERROR, AbapTagsPlugin.PLUGIN_ID, NLS.bind(
-            Messages.AbapTagsService_TagsNotSupported, project.getName()));
+        return new Status(IStatus.ERROR, AbapTagsPlugin.PLUGIN_ID, NLS.bind(Messages.AbapTagsService_TagsNotSupported,
+            project.getName()));
     }
 
     @Override
